edd66aaebe5720337f385376213a80d349e05b8d
andonokar <anderson_igmy@hotmail.com>
2025-02-08 09:33:28 -0300

    Add basic NumPy operations in basics.py
    Introduced a new script showcasing fundamental NumPy operations, including array creation, zero and one matrices, and generating sequences with `arange` and `linspace`. This serves as a foundational example for using NumPy.

---
+++ numPy/basics.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# converting list into array
+arr = np.array([1,2,3,4])
+
+# start matrix with zeroes
+zeroes = np.zeros((3,3))
+
+# start matrix with ones
+ones = np.ones((2,4))
+
+# generate array similar to range builtin
+range_array = np.arange(1, 10 , 2)
+
+# spread evenly between start and stop by a value to spread
+linspace_array = np.linspace(0, 1, 5)
+
