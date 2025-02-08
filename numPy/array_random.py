4cc2d3c09328ef9009e1088b24d20f28d628e123
andonokar <anderson_igmy@hotmail.com>
2025-02-08 12:34:25 -0300

    Add script to generate random arrays using NumPy
    Introduce a new script that creates random normalized arrays and random integer arrays using NumPy. This also sets a random seed for reproducibility.

---
+++ numPy/array_random.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# setting seeds
+np.random.seed(42)
+
+# random normalized array
+random_array = np.random.rand(3, 3)
+
+# random integers array
+random_integers = np.random.randint(0, 10, size=(2,3))
